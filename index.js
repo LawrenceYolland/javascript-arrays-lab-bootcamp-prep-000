1f426782bc2d415102ab0701b2e448af6317e207
Lawrence Yolland <lawrence@sciencedisrupt.com>
2019-04-24 19:40:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
+// Add name to end of list
 function destructivelyAppendKitten(name){
   return kittens.push(name)
 }
-
+// Add name to start of list
 function destructivelyPrependKitten(name) {
   return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
 }
